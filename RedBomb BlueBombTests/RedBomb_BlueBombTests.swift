6f86d57af11c74abd26cc5a92fd6b98dd75dad16
Samer Mujibuddin <WorkStation@samers-mbp.attlocal.net>
2016-06-13 18:45:46 -0500

    Initial Commit

---
+++ RedBomb BlueBombTests/RedBomb_BlueBombTests.swift
@@ -0,0 +1,36 @@
+//
+//  RedBomb_BlueBombTests.swift
+//  RedBomb BlueBombTests
+//
+//  Created by Samer Mujibuddin on 6/13/16.
+//  Copyright © 2016 Samer Mujibuddin. All rights reserved.
+//
+
+import XCTest
+@testable import RedBomb_BlueBomb
+
+class RedBomb_BlueBombTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
