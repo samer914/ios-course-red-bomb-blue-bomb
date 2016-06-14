75cfc7c5b0a6b7fb586a931d3bc34ce1f6f866ff
Samer Mujibuddin <WorkStation@Samers-MBP.attlocal.net>
2016-06-14 03:13:06 -0500

    added files for RedBombBlueBomb

--- RedBomb BlueBomb/ViewController.swift
+++ RedBomb BlueBomb/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//The bottom commands tie the images to the viewController
+    
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+//When you press the "Hide Blue Button", it hides the blue bomb image
+    @IBAction func HideBlue(sender: AnyObject) {
+        blueBomb.hidden = true
+    }
+//When you press the "Hide Red Button", it hides the red bomb image
+    @IBAction func HideRed(sender: AnyObject) {
+        redBomb.hidden = true
+    }
+    
+    
 }
 
